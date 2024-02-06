69ecb5b392637d692bf01ca7f971642a8e47c788
Spandanpokh <98866320+Spandanpokh@users.noreply.github.com>
2024-02-06 21:21:39 +0545

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://notes-django-afxe.onrender.com',
+    'https://notes.spandanpokhrel.com.np',
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
